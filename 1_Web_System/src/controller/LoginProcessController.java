28e1502a916b17bd4828e70c7edc2f89ffd049ec
zhantaojava <suzhantao@10.0.0.118>
2015-09-25 15:58:15 -0400

    update Controller

--- 1_Web_System/src/controller/LoginProcessController.java
+++ 1_Web_System/src/controller/LoginProcessController.java
@@ -17,7 +17,7 @@ import org.springframework.web.servlet.ModelAndView;
  * 
  */
 @Controller
-public class CopyOfLoginProcessController {
+public class LoginProcessController {
 
 	UserBeanProcess ubp = new UserBeanProcess();
 
